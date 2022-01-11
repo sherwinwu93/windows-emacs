6a5a2a70120f917d429273de11ea4719330c0855
root <root@wusd.localdomain>
2022-01-11 11:02:43 +0800

    -

--- init.el
+++ init.el
@@ -155,4 +155,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Consolas" :foundry "MS  " :slant normal :weight normal :height 97 :width normal)))))
+ '(default ((t (:family "Consolas" :foundry "MS  " :slant normal :weight normal :height 105 :width normal)))))
