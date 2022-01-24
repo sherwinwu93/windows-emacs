d91039722c7c99e2fff83389ea07919544be18d9
root <root@wusd.localdomain>
2022-01-24 15:38:16 +0800

    -

--- lisp/init-translate.el
+++ lisp/init-translate.el
@@ -1,7 +1,3 @@
 (use-package google-translate)
-(global-set-key "\C-ct" 'google-translate-at-point)
-(global-set-key "\C-cT" 'google-translate-query-translate)
-(global-set-key (kbd "C-c r") 'google-translate-at-point-reverse)
-(global-set-key (kbd "C-c R") 'google-translate-query-translate-reverse)
 
 (provide 'init-translate)
