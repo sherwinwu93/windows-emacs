74a9ccdc282f3e17d7b9b4cec266778cbb234ec5
wusd <wsd5656126@foxmail.com>
2022-08-10 21:34:30 +0800

    code

--- lisp/init-better-defaults.el
+++ lisp/init-better-defaults.el
@@ -57,7 +57,7 @@
 
 ;; ----------------------------------------文件
 ;; 更改默认目录
-(setq default-directory "d:/git-code/")
+(setq default-directory "c:/Users/wusd/codes/")
 ;; 外界修改过文件,自动加载
 (global-auto-revert-mode 1)
 ;; 关闭生成init.el~文件
